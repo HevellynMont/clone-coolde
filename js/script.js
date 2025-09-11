49653d9b054e33c7df1ec0a8dadfa75f2d055f3a
Hevellyn Monteiro <hevellynmonteiro321@gmail.com>
2025-09-11 19:24:47 -0300

    primeiros testes com javascript

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Adicionei o script.js apenas para testar se estava funcionando, não é necessário 
+var nome = window.prompt("digite outro numero: ")//var, let ou const?
+window.alert(`Seu nome tem ${nome.length} letras!`)
+console.log(`Seu nome é ${nome}`)
