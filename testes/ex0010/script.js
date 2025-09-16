dd58399c6876d961cf50bc8b2f1793059a24d8a5
Hevellyn Monteiro <hevellynmonteiro321@gmail.com>
2025-09-16 18:46:30 -0300

    coisas

---
+++ testes/ex0010/script.js
@@ -0,0 +1,9 @@
+function calcular(){
+    let velocidade = Number(window.document.querySelector('#velocidade').value)
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML += `<p>Sua velocidade atual é de <strong>${velocidade} Km/h</strong></p>`
+    if(velocidade > 60){
+        resultado.innerHTML += `<p>Velocidade acima do permitido</p>`
+    }
+    
+}
